26ac31bc74d4507d3e2f0f114fc84f950fade3af
Rafael Wrobel <rafaelwrobel2000@gmail.com>
2025-02-02 18:50:55 -0300

    testing new way to call api with env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const API_URL = `${env.VITE_API_URL ?? "http://127.0.0.1:8080"}`;
+
+  return {
+    server: {
+      proxy: {
+        "/api": API_URL,
+      },
+    },
+    plugins: [react(), TanStackRouterVite()],
+  };
 });
