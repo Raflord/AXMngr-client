42c97fbbb56d76304bf32b18609ca6fc7e9e2e2a
Rafael Wrobel <63721703+Raflord@users.noreply.github.com>
2025-02-05 08:33:24 -0300

    Update tailwind.config.js font config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import { fontFamily } from "tailwindcss/defaultTheme";
+
 export default {
   content: ["./index.html", "./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
