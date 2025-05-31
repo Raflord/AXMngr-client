147d1e2d99ef3746dabbaec32704b45fb03437e0
raflord <rafaelwrobel2000@gmail.com>
2025-05-30 21:36:41 -0300

    updating home page description

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -37,7 +37,7 @@ function Index() {
             >
               <h3 className="text-2xl font-bold">Insumos →</h3>
               <div className="text-lg">
-                Controle de insumos do almoxarifado.
+                Controle de lotes e vecimentos de insumos do almoxarifado.
               </div>
             </Link>
           </div>
