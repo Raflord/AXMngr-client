183d631e4e3335cadb3bf9c56f94f1e18cc3277a
raflord <rafaelwrobel2000@gmail.com>
2025-08-16 10:55:05 -0300

    adding eslint/prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,54 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+// @ts-nocheck
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import prettier from "eslint-plugin-prettier";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
+    files: ["**/*.{ts,tsx}"],
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: { ecmaFeatures: { jsx: true } },
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "jsx-a11y": jsxA11y,
+      prettier,
     },
+    settings: { react: { version: "detect" } },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+
+      // React Refresh (Vite/Next.js)
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
+
+      // Prettier formatting
+      "prettier/prettier": "error",
+
+      // React / JSX rules
+      "react/jsx-filename-extension": [1, { extensions: [".tsx"] }],
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "react/jsx-pascal-case": ["error", { allowAllCaps: true }],
+
+      // TypeScript rules
+      "@typescript-eslint/no-unused-vars": ["warn"],
+
+      // Naming conventions
+      camelcase: ["error", { properties: "always" }],
     },
-  },
-)
+  }
+);
